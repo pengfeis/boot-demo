fdf013f85da3a7467502112658efcb0d3d7b6dce
pengfeis <supengfei007@gmail.com>
2017-11-16 08:02:25 +0800

    add mapper and sql stmt

--- src/main/java/com/pengfeis/spr/BootDemoApplication.java
+++ src/main/java/com/pengfeis/spr/BootDemoApplication.java
@@ -1,9 +1,11 @@
 package com.pengfeis.spr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.pengfes.spr.mapper")
 public class BootDemoApplication {
 
 	public static void main(String[] args) {
