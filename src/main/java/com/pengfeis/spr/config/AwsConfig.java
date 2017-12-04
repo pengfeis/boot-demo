6cd2e99d07024621b5e7f11db47f66699c6b52a4
supengfei <supengfei@ppdai.com>
2017-12-04 18:41:13 +0800

    add spring boot profile

---
+++ src/main/java/com/pengfeis/spr/config/AwsConfig.java
@@ -0,0 +1,20 @@
+package com.pengfeis.spr.config;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class AwsConfig {
+//    @Value("${aws.credentials.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${aws.credentials.secretKey}")
+//    private String secretKey;
+//
+//    @Bean
+//    public BasicAWSCredentials basicAWSCredentials() {
+//        return new BasicAWSCredentials(accessKey, secretKey);
+//    }
+}
