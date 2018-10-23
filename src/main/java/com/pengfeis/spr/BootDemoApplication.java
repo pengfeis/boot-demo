82f2955c1a8c6738ed894918165fa56847fe29ed
pengfeis <supengfei007@gmail.com>
2018-10-23 22:38:55 +0800

    重新调整项目结构

--- src/main/java/com/pengfeis/spr/BootDemoApplication.java
+++ src/main/java/com/pengfeis/spr/BootDemoApplication.java
@@ -1,14 +1,15 @@
 package com.pengfeis.spr;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pengfeisu
+ */
 @SpringBootApplication
-@MapperScan("com.pengfeis.spr.mapper")
 public class BootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoApplication.class, args);
+    }
 }
